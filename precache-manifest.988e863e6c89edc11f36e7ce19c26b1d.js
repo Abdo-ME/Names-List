a397a8de4e156bac1f7ec41a28c1e48d256515ca
Abdo-ME <abdelhakmehda@gmail.com>
2020-10-08 17:17:07 +0100

    Updates

---
+++ precache-manifest.988e863e6c89edc11f36e7ce19c26b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3eacb5c4464f21bb24b5e87c23b59aa",
+    "url": "/Names-List/index.html"
+  },
+  {
+    "revision": "0dd076841d24cf44bd4d",
+    "url": "/Names-List/static/css/main.1769c37f.chunk.css"
+  },
+  {
+    "revision": "a903e1b8f032859213f8",
+    "url": "/Names-List/static/js/2.ffd1124d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Names-List/static/js/2.ffd1124d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dd076841d24cf44bd4d",
+    "url": "/Names-List/static/js/main.c52db4d5.chunk.js"
+  },
+  {
+    "revision": "b4734427549e3d5f7c3e",
+    "url": "/Names-List/static/js/runtime-main.4d818aa5.js"
+  }
+]);
